44020090f4ec99be398fc199cfb46f969f06b660
ak-ucs <akodooucs@gmail.com>
2026-04-29 16:19:16 +0530

    Delete fedex_shipping_odoo_integration_ucs directory

--- fedex_shipping_odoo_integration_ucs/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_company
-from . import delivery_carrier
-from . import stock_package_type
-from . import sale_order
-
--- fedex_shipping_odoo_integration_ucs/models/stock_package_type.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models, api
-
-
-class StockPackageType(models.Model):
-    _inherit = 'stock.package.type'
-    package_carrier_type = fields.Selection(selection_add=[('fdx_delivery', 'Fedex')], ondelete={'fdx_delivery': 'set default'})
